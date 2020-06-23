4fc166c7f2ebd9067a18d86e3054ff2228528840
lpita <lpita@cpqd.com.br>
2020-06-23 17:25:08 -0300

    Remove random crop due to random error and reproducibility

--- src/data_process/image_tranform.py
+++ src/data_process/image_tranform.py
@@ -7,7 +7,7 @@ IMG_STD_VALUES = (0.229, 0.224, 0.225)
 
 def transform_function():
     transform = transforms.Compose([
-        transforms.RandomCrop(IMG_SIZE),
+        #transforms.RandomCrop(IMG_SIZE),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(IMG_MEAN_VALUES,

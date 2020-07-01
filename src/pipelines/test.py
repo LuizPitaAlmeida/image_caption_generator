e24d8b07e39417daee0f147caf3832c757844643
lpita <lpita@cpqd.com.br>
2020-07-01 17:13:03 -0300

    add test code

---
+++ src/pipelines/test.py
@@ -0,0 +1,31 @@
+"""test
+Compute metrics to valid dataset
+"""
+
+import torch
+from pytorch_lightning import Trainer
+
+from models.lightining_model import ImageCaptioning
+from data_process.build_vocab import build_vocab
+
+
+class TestPrediction():
+    def __init__(self, dataloader, vocab, ckpt_path):
+        super().__init__()
+
+        self.model = ImageCaptioning(dataloader, dataloader, vocab).cuda()
+        self.ckpt_path = ckpt_path
+        self.test_data = dataloader
+
+    def notebook_test(self):
+        trainer = Trainer(resume_from_checkpoint=self.ckpt_path,
+                          gpus=1)
+        trainer.test(self.model)
+
+    def predict_in_single_batch(self):
+        checkpoint = torch.load(self.ckpt_path)
+        self.model.load_state_dict(checkpoint['state_dict'])
+        single_batch = next(iter(self.test_data))
+        imgs, captions, lengths = single_batch
+        out = self.model((imgs.cuda(), captions.cuda(), lengths))
+        return out
